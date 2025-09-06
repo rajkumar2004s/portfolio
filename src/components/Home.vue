b38e87f83d6fceca7245eeac50075ddfb75935dd
raj kumar <rajkumarbhootla@gmail.com>
2025-09-06 14:32:35 +0530

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,7 @@
       <p
         class="text-[#00ffcc] text-xl mt-[40px] tracking-wide uppercase font-semibold fade-up delay p1"
       >
-        Frontend Developer
+        A Frontend Developer
       </p>
     </div>
   </div>
